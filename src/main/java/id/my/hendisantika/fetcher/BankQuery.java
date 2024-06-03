799e25e03fcfe7d3ab36dcc4dfa5cdd3125b74fa
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 20:33:02 +0700

    Add BankQuery🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/fetcher/BankQuery.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.fetcher;
+
+import id.my.hendisantika.domain.Bank;
+import id.my.hendisantika.repository.BankRepository;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Query;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Graphql-Quarkus-Sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/3/24
+ * Time: 20:32
+ * To change this template use File | Settings | File Templates.
+ */
+@GraphQLApi
+@RequiredArgsConstructor
+public class BankQuery {
+
+    private final BankRepository repository;
+
+    @Query("banks")
+    public List<Bank> findAll() {
+        return repository.findAll().stream().collect(Collectors.toList());
+    }
+
+    @Query("bank")
+    public Bank findById(@Name("id") Long id) {
+        return repository.findById(id);
+    }
+}
