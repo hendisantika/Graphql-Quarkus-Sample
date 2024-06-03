6ef83b88a75cbdbcdc1a2c6941cf16cf76d1441d
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 20:29:02 +0700

    Add Bank Domain 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/domain/Bank.java
@@ -0,0 +1,46 @@
+package id.my.hendisantika.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Graphql-Quarkus-Sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/3/24
+ * Time: 20:28
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "bank")
+public class Bank {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String country;
+
+    @OneToMany(mappedBy = "bank")
+    private Set<User> users;
+
+    @OneToMany(mappedBy = "bank")
+    private Set<Account> accounts;
+}
